e5affd563f54e1e108da48747b933585219ca9cf
Mridul Gupta <gmridul556@gmail.com>
2023-10-17 09:01:27 +0530

    logout api

--- backend/routes.js
+++ backend/routes.js
@@ -7,5 +7,7 @@ router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
 router.post('/api/activate', authMiddleware, activateController.activate);
 router.get('/api/refresh', authController.refresh); // refresh the access token using refreshed token for autologin
+router.post('/api/logout', authMiddleware, authController.logout);
+
 
 module.exports = router;
